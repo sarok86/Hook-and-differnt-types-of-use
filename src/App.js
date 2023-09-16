e308d5640967753746ca2c943a2bd856ef82efe2
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-16 10:33:17 +0330

    useEffect and lifecycle updating

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Logger from './components/Logger';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Logger/>
+      </div>
+    );
+  }
+}
+
+export default App;
